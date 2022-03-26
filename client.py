2c5558e823ca286b983f1a143b76535077a7ece0
pravardhan <pravardhan.green1998@gmail.com>
2022-03-26 12:11:19 -0400

    Implement threading

--- client.py
+++ client.py
@@ -1,15 +1,25 @@
 import socket
+import threading
+import time
 
 HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 65432  # The port used by the server
 
+def keep_listening(conn, addr):
+    while True:
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            print("Received:", data.decode())
+        time.sleep(0.1)
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
+
+    t = threading.Thread(target=keep_listening, args=(s,""))
+    t.start()
+
     while True:
         data = input("")
-        if data == "exit":
-            break
-        s.sendall((data + "\n").encode())
-        data = s.recv(1024)
-
-        print("Received:", data.decode())
+        s.sendall(data.encode())
--- server.py
+++ server.py
@@ -1,18 +1,30 @@
 import socket
+import threading
+import time
 
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print(f"Connected by {addr}")
+def keep_listening(conn, addr):
+    while True:
         while True:
             data = conn.recv(1024)
             if not data:
                 break
             print("Received:", data.decode())
+        time.sleep(0.1)
+    
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+
+    t = threading.Thread(target=keep_listening, args=(conn,addr))
+    t.start()
+
+    with conn:
+        print(f"Connected by {addr}")
+        while True:
             data = input("")
             conn.sendall(data.encode())
