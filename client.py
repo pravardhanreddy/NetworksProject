49f558fef91dadb7ee61d89ad13b991e1c0ed3bb
pravardhan <pravardhan.green1998@gmail.com>
2022-03-31 11:34:16 -0400

    Add encryption

--- client.py
+++ client.py
@@ -1,25 +1,37 @@
 import socket
 import threading
 import time
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 
 HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 65432  # The port used by the server
 
-def keep_listening(conn, addr):
+def keep_listening(conn, decryptor):
     while True:
         while True:
             data = conn.recv(1024)
             if not data:
                 break
+            data = decryptor.update(data) + decryptor.finalize()
+            data = data[:len(data)//16]
             print("Received:", data.decode())
         time.sleep(0.1)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
 
-    t = threading.Thread(target=keep_listening, args=(s,""))
+    r = s.recv(1024)
+    key = r[:32]
+    iv = r[32:]
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv))
+    encryptor = cipher.encryptor()
+    decryptor = cipher.decryptor()
+
+    t = threading.Thread(target=keep_listening, args=(s,decryptor))
     t.start()
 
     while True:
         data = input("")
-        s.sendall(data.encode())
+        data = (data*16).encode()
+        data = encryptor.update(data) + encryptor.finalize()
+        s.sendall(data)
--- server.py
+++ server.py
@@ -1,16 +1,26 @@
 import socket
 import threading
 import time
+import os
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+
+# Encryption 
+key = os.urandom(32)
+iv = os.urandom(16)
+cipher = Cipher(algorithms.AES(key), modes.CBC(iv))
+encryptor = cipher.encryptor()
 
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
-def keep_listening(conn, addr):
+def keep_listening(conn, decryptor):
     while True:
         while True:
             data = conn.recv(1024)
             if not data:
                 break
+            data = decryptor.update(data) + decryptor.finalize()
+            data = data[:len(data)//16]
             print("Received:", data.decode())
         time.sleep(0.1)
     
@@ -19,12 +29,18 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
     conn, addr = s.accept()
+    conn.sendall(key+iv)
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv))
+    encryptor = cipher.encryptor()
+    decryptor = cipher.decryptor()
 
-    t = threading.Thread(target=keep_listening, args=(conn,addr))
+    t = threading.Thread(target=keep_listening, args=(conn,decryptor))
     t.start()
 
     with conn:
         print(f"Connected by {addr}")
         while True:
             data = input("")
-            conn.sendall(data.encode())
+            data = (data*16).encode()
+            data = encryptor.update(data) + encryptor.finalize()
+            conn.sendall(data)
