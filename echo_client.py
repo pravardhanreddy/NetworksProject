7a23372797277a197ba079bb59850f34f5354a8f
pravardhanreddy <pravardhan.green1998@gmail.com>
2022-04-25 19:42:31 -0400

    udp port prediction communication

--- echo_client.py
+++ echo_client.py
@@ -1,6 +1,10 @@
 import socket
 
+tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+tcp_sock.connect(('127.0.0.1', 9090))
+
 for i in range(3):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.sendto("Hi".encode(), ('127.0.0.1', 9999))
-    
+
+print(tcp_sock.recv(1024).decode())
--- s1.py
+++ s1.py
@@ -1,23 +1,52 @@
 import socket
 
+tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+tcp_sock.bind(('0.0.0.0', 9090))
+tcp_sock.listen()
+
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind(('0.0.0.0', 9999))
-# s.listen()
 
 l = []
+
+# Client1
+
+conn1, client1 = tcp_sock.accept()
 
 for i in range(2):
     msg, add = s.recvfrom(1024)
     l.append(add[1])
     print(add[1])
-prediction = 2 * l[1] - l[0]
+prediction1 = 2 * l[1] - l[0]
+print('prediction1:', prediction1)
 
-msg, add = s.recvfrom(1024)
-print(add[1])
+# msg, add = s.recvfrom(1024)
+# print(add[1])
 
-if prediction == add[1]:
-    print("Success")
-else:
-    print("Fail")
+# if prediction == add[1]:
+#     print("Success")
+# else:
+#     print("Fail. Predicted:", prediction)
 
-# s.close()
+# Client2
+
+conn2, client2 = tcp_sock.accept()
+
+for i in range(2):
+    msg, add = s.recvfrom(1024)
+    l.append(add[1])
+    print(add[1])
+prediction2 = 2 * l[1] - l[0]
+print('prediction2:', prediction2)
+
+# msg, add = s.recvfrom(1024)
+# print(add[1])
+
+# if prediction == add[1]:
+#     print("Success")
+# else:
+#     print("Fail. Predicted:", prediction)
+
+conn2.sendall(str(prediction1).encode())
+conn1.sendall(str(prediction2).encode())
+
